6ce283ff2c8895d3cf9f7282646b2b0c70cce7e7
Penn York <markyan@foxmail.com>
2025-06-20 01:39:50 +0800

    refactor: rename project and update constants for new CLI tool
    - Changed project name from "create-jstack-app" to "create-ystack-app" in package.json.
    - Updated default application name in constants to reflect the new project name.
    - Adjusted import statements to use Node.js module syntax for consistency.

--- cli/src/constants.ts
+++ cli/src/constants.ts
@@ -1,11 +1,11 @@
-import path from "path"
-import { fileURLToPath } from "url"
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 // With the move to TSUP as a build tool, this keeps path routes in other files (installers, loaders, etc) in check more easily.
 // Path is in relation to a single index.js file inside ./dist
-const __filename = fileURLToPath(import.meta.url)
-const distPath = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const distPath = path.dirname(__filename);
 
-export const PKG_ROOT = path.join(distPath, "../")
-export const DEFAULT_APP_NAME = "my-jstack-app"
-export const CREATE_JSTACK_APP = "create-jstack-app"
+export const PKG_ROOT = path.join(distPath, "../");
+export const DEFAULT_APP_NAME = "my-ystack-app";
+export const CREATE_JSTACK_APP = "create-jstack-app";
